877e29bbc3916c827ff0386268a01e4f3f754f47
Nganaremba <nganaremba-rem>
2023-12-18 15:54:30 +0530

    added two server with different interface

--- src/App.jsx
+++ src/App.jsx
@@ -34,29 +34,33 @@ const modelOptions = [
 
 export default function App() {
   const [searchText, setSearchText] = useState('')
+  const [endpointSearchText, setEndpointSearchText] = useState('')
   const { audioBlob, isRecording, recordNow } = useAudioRecorder()
+  const {
+    audioBlob: endpointAudioBlob,
+    isRecording: endpointIsRecording,
+    recordNow: endpointRecordNow,
+  } = useAudioRecorder()
   const [language, setLanguage] = useState('ja')
+  const [endpointLanguage, setEndpointLanguage] = useState('ja')
   const [model, setModel] = useState('base')
   const [error, setError] = useState('')
+  const [endpointError, setEndpointError] = useState('')
   const [playStartSound, { stop: stopStartSound }] = useSound(startSound)
   const [playStopSound, { stop: stopStopSound }] = useSound(stopSound)
-  const [isDigitalOceanServer, setIsDigitalOceanServer] = useState(false)
 
   const cancelTokenSourceRef = useRef(axios.CancelToken.source())
+  const endPointCancelTokenSourceRef = useRef(axios.CancelToken.source())
 
   const { mutate, isPending } = useMutation({
     mutationKey: ['postAudio'],
     mutationFn: async (myFormData) => {
       try {
-        let endpoint
-        if (isDigitalOceanServer) endpoint = '/api/transcribe'
-        else endpoint = '/api/transcribe/whisperapi'
+        let endpoint = '/api/transcribe'
+        //  endpoint = '/api/transcribe/whisperapi'
 
         const { data } = await axios.post(endpoint, myFormData, {
           cancelToken: cancelTokenSourceRef.current.token,
-          headers: {
-            Authorization: 'Bearer ' + import.meta.env.VITE_WHISPER_API_KEY,
-          },
         })
         setError('')
         return data?.text
@@ -77,12 +81,41 @@ export default function App() {
     },
   })
 
+  const { mutate: endPointMutate, isPending: endpointIsPending } = useMutation({
+    mutationKey: ['endpointPostAudio'],
+    mutationFn: async (myFormData) => {
+      try {
+        let endpoint = '/api/transcribe/whisperapi'
+
+        const { data } = await axios.post(endpoint, myFormData, {
+          cancelToken: cancelTokenSourceRef.current.token,
+          headers: {
+            Authorization: 'Bearer ' + import.meta.env.VITE_WHISPER_API_KEY,
+          },
+        })
+        setEndpointError('')
+        return data?.text
+      } catch (err) {
+        // Check if the error is due to cancellation
+        if (axios.isCancel(err)) {
+          console.log('Request canceled:', err.message)
+        } else {
+          throw new Error(err)
+        }
+      }
+    },
+    onSuccess: (data) => {
+      setEndpointSearchText(data)
+    },
+    onError: (err) => {
+      setEndpointError(err.message)
+    },
+  })
+
   useEffect(() => {
     if (audioBlob) {
       const myFormData = new FormData()
-      if (isDigitalOceanServer) {
-        myFormData.append('model', model || 'base')
-      }
+      myFormData.append('model', model || 'base')
       myFormData.append('file', audioBlob)
       myFormData.append('language', language || 'ja')
 
@@ -94,14 +127,38 @@ export default function App() {
   }, [audioBlob])
 
   useEffect(() => {
+    if (endpointAudioBlob) {
+      const myFormData = new FormData()
+      myFormData.append('file', endpointAudioBlob)
+      myFormData.append('language', endpointLanguage || 'ja')
+
+      if (endpointIsPending) {
+        endPointCancelPreviousMutation()
+      }
+      endPointMutate(myFormData)
+    }
+  }, [endpointAudioBlob])
+
+  useEffect(() => {
     setError('')
   }, [isRecording])
+
+  useEffect(() => {
+    setEndpointError('')
+  }, [endpointIsRecording])
 
   const cancelPreviousMutation = () => {
     if (cancelTokenSourceRef.current) {
       cancelTokenSourceRef.current.cancel('Previous mutation canceled')
       // Create a new CancelToken source for the next request
       cancelTokenSourceRef.current = axios.CancelToken.source()
+    }
+  }
+  const endPointCancelPreviousMutation = () => {
+    if (endPointCancelTokenSourceRef.current) {
+      endPointCancelTokenSourceRef.current.cancel('Previous mutation canceled')
+      // Create a new CancelToken source for the next request
+      endPointCancelTokenSourceRef.current = axios.CancelToken.source()
     }
   }
 
@@ -148,26 +205,12 @@ export default function App() {
             Voice to Text Transcription
           </h1>
           <div className='flex w-full space-y-10 sm:w-max flex-col  sm:justify-center  gap-3'>
-            <div className='grid space-y-5 sm:space-y-0 sm:grid-cols-[1fr_368px] sm:gap-5 sm:items-center'>
-              <label htmlFor='Server'>Server</label>
-              <div className='flex items-center'>
-                <button
-                  onClick={() => setIsDigitalOceanServer(false)}
-                  className={`flex-1 ${
-                    !isDigitalOceanServer ? 'bg-sky-600' : 'bg-gray-700'
-                  } tracking-wide rounded w-max p-2`}
-                >
-                  Whisper API Endpoint
-                </button>
-                <button
-                  onClick={() => setIsDigitalOceanServer(true)}
-                  className={`flex-1 ${
-                    isDigitalOceanServer ? 'bg-sky-600' : 'bg-gray-700'
-                  } font-bold w-max tracking-wide rounded p-2`}
-                >
-                  Digital Ocean Server
-                </button>
-              </div>
+            <div
+              title='Description: This server is configured with a local installation of Whisper API on a cloud server. It handles transcription processes, serving the resulting text.
+Implementation: Utilizes the Whisper API installed on the cloud server.'
+              className='text-center text-lg font-extrabold tracking-wide'
+            >
+              Whisper Server (Local Whisper Server)
             </div>
 
             <div className='grid space-y-5 sm:space-y-0 sm:grid-cols-[1fr_368px] sm:gap-5 sm:items-center'>
@@ -191,21 +234,19 @@ export default function App() {
                 </button>
               </div>
             </div>
-            {isDigitalOceanServer && (
-              <div className='grid sm:grid-cols-[1fr_368px] sm:gap-5 sm:items-center sm:justify-center'>
-                <label htmlFor='model'>Model</label>
-                <Select
-                  styles={colourStyles}
-                  onChange={(option) => {
-                    setModel(option?.value)
-                  }}
-                  options={modelOptions}
-                  defaultValue={() => {
-                    return modelOptions.find((option) => option.value === model)
-                  }}
-                />
-              </div>
-            )}
+            <div className='grid sm:grid-cols-[1fr_368px] sm:gap-5 sm:items-center sm:justify-center'>
+              <label htmlFor='model'>Model</label>
+              <Select
+                styles={colourStyles}
+                onChange={(option) => {
+                  setModel(option?.value)
+                }}
+                options={modelOptions}
+                defaultValue={() => {
+                  return modelOptions.find((option) => option.value === model)
+                }}
+              />
+            </div>
           </div>
 
           <div
@@ -272,6 +313,112 @@ export default function App() {
               } cursor-pointer active:bg-gray-900  pointer-events-auto ml-2  active:text-slate-200 p-2  rounded-full`}
             >
               {isRecording ? (
+                <FaStopCircle size={30} />
+              ) : (
+                <MdKeyboardVoice size={30} />
+              )}
+            </button>
+          </div>
+        </div>
+        <div className='sm:max-w-[40rem] my-5 sm:shadow-md sm:bg-gray-900 p-6 sm:p-10 rounded-xl mt-5'>
+          <div className='flex w-full space-y-10 sm:w-max flex-col  sm:justify-center  gap-3'>
+            <div
+              title={`Description: This server is acquired from whisperapi.com, providing an endpoint. A backend is required to process data, which is sent to the server's endpoint using an API key.
+Implementation: Involves setting up a backend to call the endpoint at whisperapi.com with the necessary data and API key.`}
+              className='text-center text-lg font-extrabold tracking-wide'
+            >
+              Whisper Server (Whisper Endpoint Server)
+            </div>
+
+            <div className='grid space-y-5 sm:space-y-0 sm:grid-cols-[1fr_368px] sm:gap-5 sm:items-center'>
+              <label htmlFor='language'>Language</label>
+              <div className='flex items-center'>
+                <button
+                  onClick={() => setEndpointLanguage('ja')}
+                  className={`flex-1 ${
+                    endpointLanguage === 'ja' ? 'bg-sky-600' : 'bg-gray-700'
+                  } font-bold tracking-wide rounded p-2`}
+                >
+                  日本語
+                </button>
+                <button
+                  onClick={() => setEndpointLanguage('en')}
+                  className={`flex-1 ${
+                    endpointLanguage === 'en' ? 'bg-sky-600' : 'bg-gray-700'
+                  } tracking-wide rounded p-2`}
+                >
+                  English
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className={`${
+              endpointError !== '' && !endpointIsRecording
+                ? 'visible'
+                : 'invisible'
+            } py-6 text-red-400 font-bold tracking-wide`}
+          >
+            {endpointError || 'Error'}
+          </div>
+
+          <div className=' bg-gray-800 mb-5 ring-gray-200 focus-within:ring-sky-600 pointer-events-none px-3 py-2 flex w-full items-center rounded-lg  ring-2 '>
+            <div>
+              <FaSearch size={20} className='text-slate-400' />
+            </div>
+            <input
+              value={
+                endpointIsPending
+                  ? 'Please wait...'
+                  : endpointIsRecording
+                  ? 'Recording...'
+                  : endpointSearchText
+              }
+              className={`bg-gray-800 ${
+                (endpointIsRecording || endpointIsRecording) && 'italic'
+              } w-full min-w-0 px-3 pointer-events-auto  outline-none focus:ring-cyan-400 text-xl`}
+              type='text'
+              name='search'
+              id='search'
+              readOnly={endpointIsPending || endpointIsRecording}
+              onChange={(e) => {
+                setEndpointSearchText(e.target.value)
+              }}
+            />
+            <button
+              disabled={endpointIsRecording || endpointIsPending}
+              onClick={() => {
+                if (!endpointSearchText) return
+                setEndpointSearchText('')
+              }}
+              className={`${
+                !endpointSearchText || endpointIsPending || endpointIsRecording
+                  ? 'invisible'
+                  : 'visible'
+              } border-r pointer-events-auto border-r-slate-600 p-1 hover:bg-gray-900`}
+            >
+              <MdClear size={20} />
+            </button>
+
+            <button
+              onClick={() => {
+                if (endpointIsRecording) {
+                  stopStartSound()
+                  playStopSound()
+                } else {
+                  stopStopSound()
+                  playStartSound()
+                }
+                endpointRecordNow()
+              }}
+              className={` ${
+                endpointIsRecording
+                  ? 'bg-red-600 text-white  animate-my_pulse  '
+                  : 'bg-transparent text-slate-300 bg-gray-700'
+              } cursor-pointer active:bg-gray-900  pointer-events-auto ml-2  active:text-slate-200 p-2  rounded-full`}
+            >
+              {endpointIsRecording ? (
                 <FaStopCircle size={30} />
               ) : (
                 <MdKeyboardVoice size={30} />

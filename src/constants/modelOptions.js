a76530ae5889ba761ca47849baaeda9598319553
Nganaremba <nganaremba-rem>
2024-01-09 08:17:42 +0530

    removed medium and large model, added ksw logo

--- src/constants/modelOptions.js
+++ src/constants/modelOptions.js
@@ -11,12 +11,12 @@ export const modelOptions = [
 		label: 'Small [244MB 2GB-VRAM Speed: 6x]',
 		value: 'small',
 	},
-	{
-		label: 'Medium [769MB 5GB-VRAM Speed: 2x]',
-		value: 'medium',
-	},
-	{
-		label: 'Large [1550MB 10GB-VRAM Speed: 1x]',
-		value: 'large',
-	},
+	// {
+	// 	label: 'Medium [769MB 5GB-VRAM Speed: 2x]',
+	// 	value: 'medium',
+	// },
+	// {
+	// 	label: 'Large [1550MB 10GB-VRAM Speed: 1x]',
+	// 	value: 'large',
+	// },
 ]
